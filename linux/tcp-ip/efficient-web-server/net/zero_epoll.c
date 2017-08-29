150498c36d486f29b622216b7129e2d30c2f0c1e
zjsyhjh <zjsyhjh@gmail.com>
2017-08-29 16:01:35 +0800

    modified zero_epoll

--- linux/tcp-ip/efficient-web-server/net/zero_epoll.c
+++ linux/tcp-ip/efficient-web-server/net/zero_epoll.c
@@ -1,14 +1,15 @@
 #include <stdlib.h>
 
 #include "zero_epoll.h"
-#include "../debug/debug.h"
+#include "../base/zero_debug.h"
 
 struct epoll_event *events;
 
 int zero_epoll_create(int flags) {
 	int epfd;
 	if ((epfd = epoll_create1(flags)) == -1) {
-		unix_error("zero_epoll_create() error");
+		log_err("zero_epoll_create() error");
+		exit(1);
 	}
 	events = (struct epoll_event*)malloc(sizeof(struct epoll_event) * MAX_EVENTS);
 	return epfd;
@@ -16,21 +17,24 @@ int zero_epoll_create(int flags) {
 
 void zero_epoll_add(int epfd, int fd, struct epoll_event *event) {
 	if (epoll_ctl(epfd, EPOLL_CTL_ADD, fd, events) == -1) {
-		unix_error("zero_epoll_add() error");
+		log_err("zero_epoll_add() error");
+		exit(1);
 	}
 	return;
 }
 
 void zero_epoll_mod(int epfd, int fd, struct epoll_event *event) {
 	if (epoll_ctl(epfd, EPOLL_CTL_MOD, fd, events) == -1) {
-		unix_error("zero_epoll_mod() error");
+		log_err("zero_epoll_mod() error");
+		exit(1);
 	}
 	return;
 }
 
 void zero_epoll_del(int epfd, int fd, struct epoll_event *event) {
 	if (epoll_ctl(epfd, EPOLL_CTL_DEL, fd, events) == -1) {
-		unix_error("zero_epoll_del() error");
+		log_err("zero_epoll_del() error");
+		exit(1);
 	}
 	return;
 }
@@ -38,7 +42,8 @@ void zero_epoll_del(int epfd, int fd, struct epoll_event *event) {
 int zero_epoll_wait(int epfd, struct epoll_event *events, int max_events, int timeout) {
 	int cnt;
 	if ((cnt = epoll_wait(epfd, events, max_events, timeout)) == -1) {
-		unix_error("zero_epoll_wait() error");
+		log_err("zero_epoll_wait() error");
+		exit(1);
 	}
 	return cnt;
 }
